10563070d68a5d5e9c55c4fc6efa29bf104b5285
Jorge Matos <jisapjisap@gmail.com>
2026-08-25 12:44:42 +0200

    feat: add magnetic button component with gsap integration

--- components/ui/MagneticButton.tsx
+++ components/ui/MagneticButton.tsx
@@ -4,7 +4,16 @@ import { useRef, type ReactNode } from "react";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 
-const MagneticButton = () => {
+const MagneticButton = ({
+    children,
+    className = "",
+}: {
+    children: ReactNode;
+    className?: string;
+}) => {
+
+    const ref = useRef<HTMLButtonElement>(null);
+
     return (
         <div>MagneticButton</div>
     )
